d9567d99fb8d1e9b9cd28a295d93253632c7d0e6
Alexander Naumov <alexander@imac-alexander.local>
2018-11-03 16:45:23 +0300

    Initial Commit

---
+++ ReStore/ReStore.h
@@ -0,0 +1,19 @@
+//
+//  ReStore.h
+//  ReStore
+//
+//  Created by Alexander Naumov on 03/11/2018.
+//  Copyright © 2018 Alexander Naumov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ReStore.
+FOUNDATION_EXPORT double ReStoreVersionNumber;
+
+//! Project version string for ReStore.
+FOUNDATION_EXPORT const unsigned char ReStoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ReStore/PublicHeader.h>
+
+
