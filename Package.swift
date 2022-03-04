0f0d13f4bc8c214c34a8e2cf76da4e70a1898efe
Alexander <haymob@me.com>
2022-03-04 16:35:04 +0300

    add Package.swift

---
+++ Package.swift
@@ -0,0 +1,19 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "ReStore",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(name: "ReStore", targets: ["ReStore"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/ReactiveX/RxSwift", from: "6.1.0"),
+        .package(url: "https://github.com/vadymmarkov/When", .branch("master"))
+    ],
+    targets: [
+        .target(name: "ReStore", dependencies: ["RxSwift"], path: "Sources")
+    ]
+)
